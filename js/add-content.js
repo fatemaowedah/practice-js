da69fd7dae555f6ab261f60142618623e0b7cf79
fatema Owedah <fatema.owedah@gmail.com>
2020-02-05 13:29:05 +0200

    use function

--- js/add-content.js
+++ js/add-content.js
@@ -1,12 +1,18 @@
-var today= new Date();
-var hourNow = today.getHours();
-var greeting;
-if (hourNow > 18) {
-greeting= 'Good evening!';
-}else if (hourNow > 12) {
-greeting = ' Good afternoon!';
-}else if (hourNow > 0) {
-greeting = 'Good morni ng!';
-}else {
-greeting = 'Welcome! ' ;}
-document .write( ' <h3>' +greeting + ' </ h3> '); 
+
+function thetime () {
+    // the input
+    var hourNow = prompt ("Please enter the time ,enter betwwen 0-24");
+    var greeting ="greeting";
+    // the proccess
+    if (hourNow >= 18 && hourNow > 24 ) {
+        greeting= 'Good evening!';
+        }else if (hourNow >= 12 && hourNow > 18) {
+        greeting = ' Good afternoon!';
+        }else if (hourNow >= 0 && hourNow < 12) {
+        greeting = 'Good morning!';
+        }
+        // the output
+        return greeting; 
+}
+ var hour = thetime();
+ document .write( ' <h3>' +hour + ' </ h3> ');
